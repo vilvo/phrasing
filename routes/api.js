d24891ee8593ca1d742c62d80ef39cb52e49a70b
Otto Hylli <otto.hylli@tut.fi>
2016-11-19 19:24:05 +0200

    Suorita sää- ja ilmanlaatutietojen haku rinnakkaisesti
    Käytetään async kirjaston parallel metodia suorittamaan
    tietojen haku rinnakkaisesti eikä peräkkäin kuten aiemmin.

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var async = require( 'async' );
 var ttv = require('./ttvconnector.js');
 var biomi = require( './biomiConnector.js' )
-
 
 // hae sää- ja ilmanlaatutiedot
 router.get('/', function(req, res, next) {
     var city = 'Tampere';
-  ttv.weather( city, function(err, result) {
-      biomi.getAirqualityForCity( city, function( err, biomiResult ) {
-          if ( err ) {
-              return res.send( err.message );
-          }
+    async.parallel( {
+        weather: function ( callback ) {
+            ttv.weather( city, callback );
+        },
+        airquality: function ( callback ) {
+            biomi.getAirqualityForCity( city, callback );
+        } 
+    }, function ( err, results ) {
+        if ( err ) {
+            return res.send( err.message );
+        }
           
-          result.airquality = biomiResult;
-          res.send( result );
-      })
-      
-  });
+        results.weather.airquality = results.airquality;
+        res.send( results.weather );
+    });
 });
 
 module.exports = router;
