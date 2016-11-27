ab71204c682e8723ffa92f09a9eb7902096536e3
Ville Ilvonen <ville.ilvonen@student.tut.fi>
2016-11-27 13:39:43 +0200

    phrasing-moduuli säädatatuella suomeksi. Yksikkö- ja integrointitestit.
    Signed-off-by: Ville Ilvonen <ville.ilvonen@student.tut.fi>

---
+++ routes/phrasing-fi.js
@@ -0,0 +1,88 @@
+var util = require('util');
+
+var w = "Sää tänään %s: %s %s %s, %s, %s."
+var a = "Ilmanlaatu on {0}."
+
+const yksikotmap = {
+  '0': 'nolla', '1':'yksi', '2':'kaksi', '3':'kolme', '4':'neljä',
+  '5':'viisi', '6':'kuusi', '7':'seitsämän', '8':'kahdeksan',
+  '9':'yhdeksän', '10': 'kymmenen'
+};
+const kymmenetmap = {
+  '1':'toista', '2':'kymmentä'
+};
+const fiilismap = {
+  '0':'pakkasta', '1':'lämmintä', '2':''
+};
+const tuulimap = {
+  'N':'pohjoisesta', 'NE':'koillisesta', 'E':'idästä', 'SE':'kaakosta',
+  'S':'etelästä', 'SW':'lounaasta', 'W':'lännestä', 'NW':'luoteesta'
+};
+
+function lukukirjaimiksi(luku) {
+  if (luku >= 0 && luku < 11)
+    return yksikotmap[luku];
+  else if (luku > 10 && luku < 20)
+    return yksikotmap[1] + kymmenetmap[1];
+  else if (luku >= 20 && luku < 99) {
+    var asteet = yksikotmap[Math.floor(luku/10)] + kymmenetmap[2];
+    var ykkoset = luku % 10;
+    if (ykkoset > 0)
+      asteet = asteet + yksikotmap[ykkoset];
+    return asteet;
+  }
+  return undefined;
+}
+
+function asteita(lampo) {
+  var a = "aste";
+  if (Math.abs(lampo) == 1)
+    return a
+  else
+    return a + "tta"
+}
+
+function taivutusmuoto(kaupunki) {
+  if (kaupunki == 'Tampere')
+      return kaupunki + "ella"
+  else if (kaupunki.indexOf('Turku') != -1)
+      return "Turussa"
+}
+
+function lampotila(lampo) {
+    if (lampo < 0)
+      return fiilismap[0];
+    else if (lampo > 0)
+      return fiilismap[1];
+    else
+      return fiilismap[2];
+}
+
+function tuuli(speed, direction) {
+  if (speed == 0)
+    return "tyyntä";
+  else
+    return util.format("tuuli %s %s metriä sekunnissa",
+                        tuulimap[direction], yksikotmap[speed]);
+}
+
+function pilvisyys(clouds) {
+  if (clouds.indexOf('melk.') != -1)
+    return clouds.replace('melk.', 'melko ')
+  else
+    return clouds;
+}
+
+var phrase = function (weather, airquality) {
+  if (weather == undefined && airquality == undefined)
+    return undefined;
+  return util.format(w, taivutusmuoto(weather.city),
+                        lukukirjaimiksi(Math.abs(weather.temperature)),
+                        asteita(weather.temperature),
+                        lampotila(weather.temperature),
+                        tuuli(weather.windSpeed, weather.windDirection),
+                        pilvisyys(weather.clouds));
+};
+
+module.exports.phrase = phrase;
+module.exports.lukukirjaimiksi = lukukirjaimiksi;
---
+++ test/test-phrasing.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+var phrasing = require('../routes/phrasing-fi.js');
+
+describe('phrasing', function() {
+  describe('#phrase undefined, undefined', function() {
+    it('should phrase undefined for undefined, undefined', function(){
+        assert.equal(undefined, phrasing.phrase(undefined, undefined));
+    });
+  });
+  describe('#phrase luku kirjaimiksi', function(){
+    it('should phrase 1 to "yksi"', function() {
+      assert.equal("yksi", phrasing.lukukirjaimiksi(1));
+    });
+    it('should phrase 11 to "yksitoista"', function() {
+      assert.equal("yksitoista", phrasing.lukukirjaimiksi(11));
+    });
+    it('should phrase 20 to "kaksikymmentä"', function() {
+      assert.equal("kaksikymmentä", phrasing.lukukirjaimiksi(20));
+    });
+    it('should phrase 33 to "kolmekymmentäkolme"', function() {
+      assert.equal("kolmekymmentäkolme", phrasing.lukukirjaimiksi(33));
+    });
+    it('should phrase 50 to "viisikymmentä"', function() {
+      assert.equal("viisikymmentä", phrasing.lukukirjaimiksi(50));
+    });
+    it('should phrase 0 to "nolla"', function() {
+      assert.equal("nolla", phrasing.lukukirjaimiksi(0));
+    });
+    it('should phrase 100 to undefined', function() {
+      assert.equal(undefined, phrasing.lukukirjaimiksi(100));
+    });
+  });
+  describe('#phrase Tampere with const weather input data', function(){
+    it('should phrase with const input data T1', function() {
+      const T1 = {
+          temperature: -1,
+          windDirection: 'NW',
+          windSpeed: 4,
+          city: 'Tampere',
+          clouds: 'lumisadetta'
+      };
+      const s = "Sää tänään Tampereella: yksi aste pakkasta, " +
+                "tuuli luoteesta neljä metriä sekunnissa, lumisadetta."
+      assert.equal(s, phrasing.phrase(T1), undefined);
+    });
+    it('should phrase with const input data T2', function() {
+      const T2 = {
+          temperature: -1,
+          windDirection: 'N',
+          windSpeed: 6,
+          city: 'Turku',
+          clouds: 'melk.pilvistä'
+      };
+      const s = "Sää tänään Turussa: yksi aste pakkasta, " +
+                "tuuli pohjoisesta kuusi metriä sekunnissa, melko pilvistä."
+      assert.equal(s, phrasing.phrase(T2), undefined);
+    });
+  });
+});
