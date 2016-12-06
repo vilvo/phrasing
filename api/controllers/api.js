a1e2e7cc40bd70ea9e6d74353227f54650e80c45
Otto Hylli <otto.hylli@tut.fi>
2016-12-06 16:16:48 +0200

    Lisätty kommentteja koodiin

--- api/controllers/api.js
+++ api/controllers/api.js
@@ -3,9 +3,12 @@ var ttv = require('./ttvconnector.js');
 var biomi = require('./biomiConnector.js');
 var phrasing = require('./phrasing-fi.js');
 
-// hae sää- ja ilmanlaatutiedot
+// hae sää- ja ilmanlaatutiedot annetulle kaupungille
 function getData(req, res) {
+    // kaupungin nimi on URL:in polussa
     var city = req.swagger.params.city.value;
+    // haetaan sää- ja ilmanlaatutiedot  rinakkain ja käsitellään tulos
+    // kun molemmat on haettu
     async.parallel( {
         weather: function ( callback ) {
             ttv.weather( city, callback );
@@ -15,17 +18,23 @@ function getData(req, res) {
         }
     }, function ( err, results ) {
         if ( err ) {
+            // jompi kumpi operaatio epäonnistui
+            // katsotaan koko homma epäonnistuneeksi
             return res.send( { message: err.message });
         }
-
+        
+        // results.weather ja results.airquality sisältää rinnakkain ajettujen funktioiden tulokset
+        
         if ( results.airquality.length === 0 ) {
+            // paikkakunnalta ei ole saatavissa ilmanlaatutietoa
             results.weather.hasAirquality = false;
         }
 
         else {
             results.weather.hasAirquality = true;
         }
-
+        
+        // results.weather palautetaan käyttäjälle
         results.weather.airquality = results.airquality;
 
         // tarkista jos ei löydetty kumpiakaan tietoja
@@ -33,7 +42,8 @@ function getData(req, res) {
             results.weather.phrase = "Tietoja ei löytynyt kaupungille " +city +".";
             return res.send( results.weather );
         }
-
+        
+        // rakenna tiedoista lause
         results.weather.phrase = phrasing.phrase(results.weather,
                                                  results.airquality);
         res.send( results.weather );
